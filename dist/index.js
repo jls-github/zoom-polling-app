32597f59ba847e9d6dc73bd564731f831222e412
John <souzajohnlee@gmail.com>
2021-02-19 13:14:41 -0800

    set up ejs files and dependencies

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const port = 8080;
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.render("index");
+});
+app.post("/polls", (req, res) => {
+    res.send("post new poll");
 });
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
+import path from "path";
+
 const app = express();
 const port = 8080;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render("index");
 });
 
 app.post("/polls", (req, res) => {
-  res.send("post new poll");    
+  res.send("post new poll");
 });
 
 app.listen(port, () => {
