d0f88aa235bc079528eaceaabbd6da6e8dec4e31
John <souzajohnlee@gmail.com>
2021-02-21 00:22:05 -0800

    expect json response

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,16 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   if (req.query.code) {
     getAccessToken(req.query.code as string)
-    .then(data => {
-      res.render("index", {accessToken: data["access_token"], scope: data["scope"]})
-    })
-    .catch(error => {
-      console.log(error)
-      res.send("hello world")
-    })
+      .then((data) => {
+        res.render("index", {
+          accessToken: data["access_token"],
+          scope: data["scope"],
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.send("hello world");
+      });
   } else {
     res.redirect(zoomRedirectUrl);
   }
@@ -51,17 +54,22 @@ app.listen(port, () => {
 });
 
 async function getAccessToken(oauthCode: string): Promise<Record<string, any>> {
-  console.log("hello-----------")
+  console.log("hello-----------");
   const res = await axios.get(
     `https://zoom.us/oauth/token?grant_type=authorization_code&code=${oauthCode}&redirect_uri=https://zoom-poller.herokuapp.com/`,
-    { headers: { "Authorization": `Basic ${oauthCode}` } }
+    {
+      headers: {
+        Authorization: `Basic ${oauthCode}`,
+        Accept: "application/json",
+      },
+    }
   );
-  console.log('hello2----------')
-  console.log(res)
+  console.log("hello2----------");
+  console.log(res);
   const data = res.data;
   if (data["access_token"]) {
     return { access_token: data["access_token"], scope: data["scope"] };
   } else {
-    return {"error": "Could not fetch access token"};
+    return { error: "Could not fetch access token" };
   }
 }
